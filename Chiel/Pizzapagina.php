b41b894ff0cbc34c263a3feaeebd125dc4a33cc4
chielkroeze <c.r.kroeze@st.hanze.nl>
2016-11-07 16:38:12 +0100

    Update Pizzapagina.php

--- Chiel/Pizzapagina.php
+++ Chiel/Pizzapagina.php
@@ -4,11 +4,11 @@
 
 <!-- Chiel 02/11/16 --> 
 <center>
-    <a href="Pizzahawai.php"><img src ="hawai.jpg" align="middle" hspace=3" vspace="3 width="77" height="100" border="1">Pizza hawai</a>
-    <a href="Pizzamargherita.php"><img src ="margherita.jpg" align="middle" hspace=3" vspace="3 width="77" height="100" border="1">Pizza margherita</a>
-    <a href="Pizzapepperoni.php"><img src ="pepperoni.jpg" align="middle" hspace=3" vspace="3 width="77" height="100" border="1">Pizza pepperoni</a>
-    <a href="Pizzapollo.php"><img src ="pollo.jpg" align="middle" hspace=3" vspace="3 width="77" height="100" border="1">Pizza Pollo</a>
-    <a href="Pizzasalami.php"><img src ="salami.jpg" align="middle" hspace=3" vspace="3 width="77" height="100" border="1">Pizza Salami</a>
+    <a href="Pizzahawai.php"><img src ="images/hawai.jpg" align="middle" hspace=3" vspace="3 width="77" height="100" border="1">Pizza hawai</a>
+    <a href="Pizzamargherita.php"><img src ="images/margherita.jpg" align="middle" hspace=3" vspace="3 width="77" height="100" border="1">Pizza margherita</a>
+    <a href="Pizzapepperoni.php"><img src ="images/pepperoni.jpg" align="middle" hspace=3" vspace="3 width="77" height="100" border="1">Pizza pepperoni</a>
+    <a href="Pizzapollo.php"><img src ="images/pollo.jpg" align="middle" hspace=3" vspace="3 width="77" height="100" border="1">Pizza Pollo</a>
+    <a href="Pizzasalami.php"><img src ="images/salami.jpg" align="middle" hspace=3" vspace="3 width="77" height="100" border="1">Pizza Salami</a>
     
 </center>
 </body>
