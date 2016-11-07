328c54a03f41918f04b651c35773c4989363f4c3
chielkroeze <c.r.kroeze@st.hanze.nl>
2016-11-07 16:15:26 +0100

    Create Menukaart.php

---
+++ Chiel/Menukaart.php
@@ -0,0 +1,6 @@
+<?php 
+include 'NaviNoScroll.php'; 									//Roept de navigatiebalk aan
+include 'Homepage Slideshow.php';						//Roept de slideshow aan
+include 'Pizzapagina.php';					//Roept de informatie van 
+include 'Footer.php';									//Roept Footer aan
+?>
