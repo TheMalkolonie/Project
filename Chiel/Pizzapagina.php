d78f9b9756c60195b173c61e5cbda541e33283b3
chielkroeze <c.r.kroeze@st.hanze.nl>
2016-11-07 16:15:58 +0100

    Create Pizzapagina.php

---
+++ Chiel/Pizzapagina.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<!-- Chiel 02/11/16 --> 
+<center>
+    <a href="Pizzahawai.php"><img src ="hawai.jpg" align="middle" hspace=3" vspace="3 width="77" height="100" border="1">Pizza hawai</a>
+    <a href="Pizzamargherita.php"><img src ="margherita.jpg" align="middle" hspace=3" vspace="3 width="77" height="100" border="1">Pizza margherita</a>
+    <a href="Pizzapepperoni.php"><img src ="pepperoni.jpg" align="middle" hspace=3" vspace="3 width="77" height="100" border="1">Pizza pepperoni</a>
+    <a href="Pizzapollo.php"><img src ="pollo.jpg" align="middle" hspace=3" vspace="3 width="77" height="100" border="1">Pizza Pollo</a>
+    <a href="Pizzasalami.php"><img src ="salami.jpg" align="middle" hspace=3" vspace="3 width="77" height="100" border="1">Pizza Salami</a>
+    
+</center>
+</body>
+</html>
