78a8b99f227fff92f17406aed0b6fb630009808e
SebasJH <SebasJHartman@gmail.com>
2016-11-07 15:50:57 +0100

    Update Footer.php

--- Sebas/Footer.php
+++ Sebas/Footer.php
@@ -1,37 +1,11 @@
 <!-- De footer -->
 
 <HTML>
-<h6>
 </body>
 
-<div class="container">
-    <div class="main">
-        
-    </div>
-</div>
-<div class="clear"></div>
-    <div id="footer">
-        <div class="container">
-            <div class="footer_nav">
-                <ul>
-                    <li><font color="white"><p align="left">Groningen </br>
-                    	Zernikeplein 7 </br> 
-                    	9747 AS</br>
-                    	Pizzaenco@pizza.com</br>
-                    	  </li>                                      
-                </ul>
-            </div>
-           
-                
-            </div>
-        </div>
-        <div class="clear"></div>
-    </div>
+<br/><br/><br/>
+<font size="3" face="arial">Groningen<br/>Zernikeplein 7<br/>9747 AS<br/>PizzaenCo@Pizza.nl</font></br>
+
 </body>
-
-</h6>
-<!-- Einde van footer -->
-
-
 </html>
-<!-- Einde van footer -->
+
