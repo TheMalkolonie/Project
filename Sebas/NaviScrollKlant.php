dc5f011a16496143ba5538a16b20ec5017f3e7a4
SebasJH <SebasJHartman@gmail.com>
2016-11-08 15:48:47 +0100

    Update NaviScrollKlant.php

--- Sebas/NaviScrollKlant.php
+++ Sebas/NaviScrollKlant.php
@@ -45,19 +45,19 @@ li a:hover:not(.active) {
 
 
 <ul>
-  <li><a href="HomePageKlant.php"><img src="Pizza & Co Logo.png" alt="Pizza & Co" style="width:85px;height:85px;border:0;"></a></li> 
+  <li><a href="index.php"><img src="images/Pizza & Co Logo.png" alt="Pizza & Co" style="width:85px;height:85px;border:0;"></a></li> 
   <li><a><font size="7" face="arial" color="">Pizza & Co</font></a></li>
   <li><a><font size="4" face="arial">&emsp;&emsp;&emsp;&emsp;&emsp;</font></a></li>
  
-  <li><a href="HomePageKlant.php"><font size="7" face="arial">Home</font></a></li>
-  <li><a href="PizzaPageKlant.php"><font size="7" face="arial">Pizza's</font></a></li>
-  <li><a href="DessertPageKlant.php"><font size="7" face="arial">Desserts</font></a></li>
-  <li><a href="DrankenPageKlant.php"><font size="7" face="arial">Dranken</font></a></li>
-  <li><a href="SnacksPageKlant.php"><font size="7" face="arial">Snacks</font></a></li>
+  <li><a href="index.php"><font size="7" face="arial">Home</font></a></li>
+  <li><a href="Menukaart.php"><font size="7" face="arial">Pizza's</font></a></li>
+  <li><a href="DessertPage.php"><font size="7" face="arial">Desserts</font></a></li>
+  <li><a href="DrankenPage.php"><font size="7" face="arial">Dranken</font></a></li>
+  <li><a href="SnacksPage.php"><font size="7" face="arial">Snacks</font></a></li>
   
-   <<li><a><font size="4" face="arial">&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;</font></a></li>
+    <li><a><font size="4" face="arial">&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;</font></a></li>
    <li><a href="logout.php"><font size="4" face="arial">Uitloggen</font></a></li>
-   <li><a href="Winkelmandje.php"><img src="winkelmandje plaatje.png" alt="Pizza & Co" style="width:35px;height:35px;border:0;"></a></li> 
+   <li><a href="Winkelmandje.php"><img src="images/winkelmandje plaatje.png" alt="Pizza & Co" style="width:30px;height:30px;border:0;"></a></li> 
    
 </ul>
 
