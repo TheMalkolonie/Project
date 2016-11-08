c89f79ae6e036a35e22ee2a7dfd1a5845a3f2941
SebasJH <SebasJHartman@gmail.com>
2016-11-08 15:49:24 +0100

    Update NaviNoScrollKlant.php

--- Sebas/NaviNoScrollKlant.php
+++ Sebas/NaviNoScrollKlant.php
@@ -10,9 +10,7 @@ ul {
     padding: 0px 0px 0px 0px;
     overflow: hidden;
     background-color: #333;
-    position: fixed;
-    top: 0;
-    width: 100%;			
+	
 }
 
 li {
@@ -22,7 +20,8 @@ li {
 li a {
     display: block;
     color: white;
-    padding: 16px;
+    text-align: center;
+    padding: 14px 16px;
     text-decoration: none;
 }
 
@@ -33,32 +32,25 @@ li a:hover:not(.active) {
 .active {
     background-color: #4CAF50;
 }
-
-.main {
-    padding: 16px;
-    margin-top: 30px;
-    height: 1500px; /* Used in this example to enable scrolling */
-}
 </style>
 </head>
 <body>
 
 
 <ul>
-  <li><a href="HomePageKlant.php"><img src="Pizza & Co Logo.png" alt="Pizza & Co" style="width:85px;height:85px;border:0;"></a></li> 
+  <li><a href="index.php"><img src="images/Pizza & Co Logo.png" alt="Pizza & Co" style="width:85px;height:85px;border:0;"></a></li> 
   <li><a><font size="7" face="arial" color="">Pizza & Co</font></a></li>
   <li><a><font size="4" face="arial">&emsp;&emsp;&emsp;&emsp;&emsp;</font></a></li>
  
-  <li><a href="HomePageKlant.php"><font size="7" face="arial">Home</font></a></li>
-  <li><a href="PizzaPageKlant.php"><font size="7" face="arial">Pizza's</font></a></li>
-  <li><a href="DessertPageKlant.php"><font size="7" face="arial">Desserts</font></a></li>
-  <li><a href="DrankenPageKlant.php"><font size="7" face="arial">Dranken</font></a></li>
-  <li><a href="SnacksPageKlant.php"><font size="7" face="arial">Snacks</font></a></li>
+  <li><a href="index.php"><font size="7" face="arial">Home</font></a></li>
+  <li><a href="Menukaart.php"><font size="7" face="arial">Pizza's</font></a></li>
+  <li><a href="DessertPage.php"><font size="7" face="arial">Desserts</font></a></li>
+  <li><a href="DrankenPage.php"><font size="7" face="arial">Dranken</font></a></li>
+  <li><a href="SnacksPage.php"><font size="7" face="arial">Snacks</font></a></li>
   
-   <<li><a><font size="4" face="arial">&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;</font></a></li>
+   <li><a><font size="4" face="arial">&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;</font></a></li>
    <li><a href="logout.php"><font size="4" face="arial">Uitloggen</font></a></li>
-   <li><a href="Winkelmandje.php"><img src="winkelmandje plaatje.png" alt="Pizza & Co" style="width:35px;height:35px;border:0;"></a></li> 
-   
+   <li><a href="Winkelmandje.php"><img src="images/winkelmandje plaatje.png" alt="Pizza & Co" style="width:30px;height:30px;border:0;"></a></li> 
 </ul>
 
 
