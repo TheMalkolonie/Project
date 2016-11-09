8a4df58bccc821060ce9533c5faa3d9663ee2ed5
SebasJH <SebasJHartman@gmail.com>
2016-11-09 13:39:05 +0100

    Create LogInButton.php

---
+++ Sebas/LogInButton.php
@@ -0,0 +1,33 @@
+<hmtl>
+<head>
+<style>
+input.MyButton {
+width: 300px;
+padding: 20px;
+cursor: pointer;
+font-size: 175%;
+background: #3366cc;
+color: #fff;
+border: 1px solid #3366cc;
+border-radius: 10px;
+-moz-box-shadow:: 6px 6px 5px #999;
+-webkit-box-shadow:: 6px 6px 5px #999;
+box-shadow:: 6px 6px 5px #999;
+}
+input.MyButton:hover {
+color: #fff;
+background: #1b379b;
+border: 1px solid #fff;
+-moz-box-shadow:: 5px 5px 4px #adadad;
+-webkit-box-shadow:: 5px 5px 4px #adadad;
+box-shadow:: 5px 5px 4px #adadad;
+} </style>
+</head>
+
+
+<center>
+<form>
+<input class="MyButton" type="button" value="Log in" onclick="window.location.href='klogin.php'" />
+</form>
+</center>
+</html>
