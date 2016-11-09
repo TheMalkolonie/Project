497c445e2efdec16e20c968589227b9ea9423761
SebasJH <SebasJHartman@gmail.com>
2016-11-09 13:38:18 +0100

    Update Winkelmandje.php

--- Sebas/Winkelmandje.php
+++ Sebas/Winkelmandje.php
@@ -1,7 +1,28 @@
-<?php	include 'NaviScrollKlant.php';?>
-		<img src="Pizza Plaatje.jpg" alt="Pizza" style="width:1920px;height:675px;">
+<?php
+session_start();				//Als klant is ingelogd, wordt 'NaviScrollKlant.php' gestart. Zo niet verschijnt 'NaviScroll.php'
+if(!isset($_SESSION['login2'])){
+	include 'NaviScroll.php';
+}
+Else{
+	include 'NaviScrollKlant.php';
+} ?>
+<html>
+		<img src="PLAATJE VAN PIZZA" alt="Pizza" style="width:1920px;height:50%;">
 		<center><h2><font size="6" face="arial">Mijn winkelmandje</font></h2></center>
-<center><font size="3" face="arial">DATABASE CONNECT WINKELMANDJE<br/></font></center>
+<center><font size="3" face="arial">ITEMS HIER<br/></font></center>
+</html>
+<br/><br/><br/><br/>
 
-    
-<?php	include 'Footer.php';?>
+<?php
+session_start();							//Als klant is ingelogd, kan hij klikken op bestel nu, als hij niet is ingelogd staat er log in.
+if(!isset($_SESSION['login2'])){
+	include 'BestelButton.php';
+}
+Else{
+	include 'LogInButton.php';
+}
+
+
+
+include 'Footer2.php';						//De footer wordt opgehaald
+?> 		
