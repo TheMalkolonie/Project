f28e92cda60ca583a10697ef8a39373a4b026795
SebasJH <SebasJHartman@gmail.com>
2016-11-11 12:15:05 +0100

    Update Homepage informatie.php

--- Website/FINAL/Homepage informatie.php
+++ Website/FINAL/Homepage informatie.php
@@ -1,5 +1,5 @@
-<html>
-<body>
+<html>			
+<body>				<!-- Hier wordt de informatie beschreven voor op de homepage  -->
 <center><h2><font size="6" face="arial">Info</font></h2></center>
 <center><font size="3" face="arial">Alle informatie van Pizza & Co komt hier te staan.<br/> 
 									Welkom op de website van Pizza & Co!<br/>														
