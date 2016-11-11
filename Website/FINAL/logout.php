5f2ebeec51daaa789231a7a2f6335c4b339f5a03
TimdeRaaf <t.a.de.raaf@st.hanze.nl>
2016-11-11 12:21:10 +0100

    Update logout.php

--- Website/FINAL/logout.php
+++ Website/FINAL/logout.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-session_destroy();
+session_destroy(); //sesion destroy eindigd de sessie en zorgt ervoor dat je uitlogt.
 echo '<script language="javascript">';
 echo 'alert("U bent nu uigelogd.")';
 echo '</script>';
